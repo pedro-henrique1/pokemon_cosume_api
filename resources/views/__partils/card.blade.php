7e1889f91e98e91ffb63d81fe9ed9aa1bd5b56fc
pedro henrique <ph8464803@gmail.com>
2022-01-17 18:28:31 -0300

    feature: front end terminado

--- resources/views/__partils/card.blade.php
+++ resources/views/__partils/card.blade.php
@@ -8,8 +8,12 @@
             alt=" {{ $responseBody->name }}">
         <div class="card-body">
             <h3 class="card-text text-center">{{ $responseBody->name }}</h3>
-            <p class="card-text text-center mt-2">Tipo: {{ $responseBody->types[0]->type->name }}
-            </p>
+            <p class="card-text text-center mt-2">type: {{ $responseBody->types[0]->type->name }}</p>
+            <p class="card-text text-center mt-4 ">HP: {{ $responseBody->stats[0]->base_stat }}</p>
+            <p class="card-text text-center mt-2">Attack: {{ $responseBody->stats[1]->base_stat }}1</p>
+            <p class="card-text text-center mt-2">Desfense: {{ $responseBody->stats[2]->base_stat }}</p>
+            <p class="card-text text-center mt-2">Special Attack: {{ $responseBody->stats[3]->base_stat }}</p>
+            <p class="card-text text-center mt-2">Speed: {{ $responseBody->stats[4]->base_stat }}</p>
         </div>
     </div>
 @endif
